662a4a655aa491818afd22993b85174e42c5e5ff
Sofia Saraiva <112433998+Sofia-Saraiva@users.noreply.github.com>
2023-05-20 14:50:41 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,14 +2,20 @@ import sys
 import os
 import transacao
 
+# verifica se arquivo existe, se nao cria um
+try:
+  transacao.arquivar()
+except FileNotFoundError:
+  transacao.criar_arquivo()
+  
 print("----------------------- MENU -----------------------")
-print("[LER] Extrato\n[ADD] Adicionar transação\n[ATT] Atualizar transação\n[DEL] Deletar transação\n[BUD] Budget")
-transacao.arquivar()
+print("[LER]   Extrato\n[ADD]   Adicionar transação\n[ATT]   Atualizar transação\n[DEL]   Deletar transação\n[BUD]   Budget")
 
 acao = input().upper()
 if acao == 'LER':
+  # pede input do user ate que seja igual a 1 ou 2. mostra o extrato ou filtra por categoria
   while True:
-    tipo = int(input("1 - Extrato\n2 - Filtrar por categoria\n"))
+    tipo = int(input("[1]   Extrato\n[2]   Filtrar por categoria\n"))
     try:
       if tipo == 1:
         transacao.extrato()
@@ -23,19 +29,23 @@ if acao == 'LER':
       print("Escolha uma das opções.")
       
 elif acao == 'ADD':
+  # adiciona a transação e arquiva ela nos dados
   transacao.add()
   transacao.arquivar()
   
 elif acao == 'ATT':
+  # atualiza a transação e arquiva ela nos dados
   transacao.atualizar()
   transacao.arquivar()
   
 elif acao == 'DEL':
+  # deleta a transação (tira a transação dos dados e reescreve o arquivo csv), apaga a lista de dados e usa a função arquivar para ler o arquivo e puxar as transaçoes pra lista
   transacao.deletar()
   dados = []
   transacao.arquivar()
 
 elif acao == 'BUD':
+  # pede o orçamento do user, dá o valor total gasto e o valor poupado ate o momento
   transacao.budget()
 else:
   print("Função não existente.")
